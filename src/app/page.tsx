ec50ff7a70436fea23285b3f34c836cb60c4393e
RalfiBahar <ralfibahar2005@hotmail.com>
2024-09-10 19:46:11 -0700

    feat: show loading screen until isMobile determined

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
 "use client";
+import React, { useState, useEffect } from "react";
 import Home from "./dynamicPages/Home";
 import HomeMobile from "./dynamicPages/HomeMobile";
 import { useWindowSize } from "./hooks/useWindowSize";
+import { CircularProgress } from "@mui/material";
 
 export default function Index() {
   const { width } = useWindowSize();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isMobile, setIsMobile] = useState(false);
 
-  const isMobile = width && width < 768;
+  useEffect(() => {
+    if (width) {
+      setIsMobile(width < 768);
+      setIsLoading(false);
+    }
+  }, [width]);
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-[#68EAAC]">
+        <CircularProgress sx={{ color: "#000" }} />
+      </div>
+    );
+  }
 
   return <div>{isMobile ? <HomeMobile /> : <Home />}</div>;
 }
